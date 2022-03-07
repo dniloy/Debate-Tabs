1e8ad65fd9c454b6bca7c885720e064c93827e38
dniloy <dihan8917@gmail.com>
2022-03-07 14:17:03 -0500

    Updated analyze_team_positions.py
    - Get average scores for each team position in a round
    - Misc. functions to give useful information like total number of rounds in scraped data
    - Made team_positions_stats take and return lists of dfs, instead of directly analysing files and printing results

--- analyze_team_positions.py
+++ analyze_team_positions.py
@@ -2,43 +2,46 @@
 import pandas as pd
 import os
 import re
+import validators
+
 from pathlib import Path
+from typing import List, Tuple, Set
 
 
-def get_team_positions_list() -> None:
+def team_positions_stats(results_dfs: List[pd.DataFrame]) -> Tuple[tuple, tuple, tuple]:
     two_teams_positions_list = [0, 0]  # list for two-team formats e.g. Australs
     four_teams_positions_list = [0, 0, 0, 0]  # list for four-team formats e.g. BP
-    set_of_team_positions = set()  # to find the unique list of team positions
 
-    # get filepath for scraped data directory
-    filepath = Path('scraped_data/')
-    filepath.parent.mkdir(parents=True, exist_ok=True)
-    for file in os.listdir(filepath):  # iterate through all the scraped data
-        if file[-14:] == ' - Results.csv':  # if results file
-            # find the file's filepath and get the dataframe
-            filepath = Path('scraped_data/' + file)
-            filepath.parent.mkdir(parents=True, exist_ok=True)
-            results_df = pd.read_csv(filepath)
+    for results_df in results_dfs:  # for each results dataframe
+        for index, row in results_df.iterrows():  # for every room in the file
 
-            for room in results_df['Rankings']:  # for every room in the file
-                teams = room.split('\', \'')  # split teams by commas
-                teams[0] = teams[0][2:]  # remove the [' at the start
-                teams[-1] = teams[-1][:-2]  # remove the '] at the end
-                for team in teams:  # for each team in the room
-                    position = re.search(r'\([A-Z]+\)', team[-4:])  # find team position substring
-                    # add the score in team[0] to the appropriate list slot
-                    if position in {'(P)', '(G)', '(A)'}:
-                        two_teams_positions_list[0] += int(team[0])
-                    elif position in {'(O)', '(N)'}:
-                        two_teams_positions_list[1] += int(team[0])
-                    elif position == '(OG)':
-                        four_teams_positions_list[0] += int(team[0])
-                    elif position == '(OO)':
-                        four_teams_positions_list[1] += int(team[0])
-                    elif position == '(CG)':
-                        four_teams_positions_list[2] += int(team[0])
-                    elif position == '(CO)':
-                        four_teams_positions_list[3] += int(team[0])
+            room = row['Rankings']
+            teams = room.split('\', \'')  # split teams by commas
+            teams[0] = teams[0][2:]  # remove the [' at the start
+            teams[-1] = teams[-1][:-2]  # remove the '] at the end
+            for team in teams:  # for each team in the room
+                # find team position substring
+                position = re.search(r'\([A-Z]+\)', team[-4:]).group()
+                # add the score in team[0] to the appropriate list slot
+                if position in {'(P)', '(G)', '(A)'}:
+                    two_teams_positions_list[0] += int(team[0])
+                elif position in {'(O)', '(N)'}:
+                    two_teams_positions_list[1] += int(team[0])
+                elif position == '(OG)':
+                    four_teams_positions_list[0] += int(team[0])
+                elif position == '(OO)':
+                    four_teams_positions_list[1] += int(team[0])
+                elif position == '(CG)':
+                    four_teams_positions_list[2] += int(team[0])
+                elif position == '(CO)':
+                    four_teams_positions_list[3] += int(team[0])
+                else:
+                    print(position)
+
+    if two_teams_positions_list == [0, 0]:
+        two_teams_positions_list = []
+    if four_teams_positions_list == [0, 0, 0, 0]:
+        four_teams_positions_list = []
 
     # get average team position scores for two team format
     normalized_two = [score * 3 / (two_teams_positions_list[0] + two_teams_positions_list[1])
@@ -50,32 +53,83 @@ def get_team_positions_list() -> None:
     # get average side scores for four team format
     normalized_bp_two = [(normalized_four[0] + normalized_four[2]) / 2,
                          (normalized_four[1] + normalized_four[3]) / 2]
-    # print stats
-    print(normalized_two)
-    print(normalized_four)
-    print(normalized_bp_two)
+
+    return tuple(normalized_two), tuple(normalized_four), tuple(normalized_bp_two)
 
 
-def count_debates(folder: str) -> int:
+def round_by_round_stats(results_dfs: List[pd.DataFrame]) -> List[tuple]:
+    rounds_stats = []
+    for round in results_dfs:
+        rounds_stats.append(team_positions_stats([round]))
+    return rounds_stats
+
+
+def unique_round_names(results_dfs: List[pd.DataFrame]) -> Set[str]:
+    # get filepath for scraped data directory
+    unique_round_names = set()
+
+    for results_df in results_dfs:
+        # if finals round
+        names = {round for round in results_df['Round Name']
+                 if ('nal' in round or " GF" in round or 'emi' in round)}
+        # unique_round_names = unique_round_names.union(set(results_df['Round Name']))
+        unique_round_names = unique_round_names.union(names)
+
+    return unique_round_names
+
+
+def count_debates(results_dfs: List[pd.DataFrame]) -> int:
     """Count total number of debates in the given folder.
 
     Preconditions
         - folder starts without '/' and ends with '/'"""
     # find folder containing debates data
-    filepath = Path(folder)
-    filepath.parent.mkdir(parents=True, exist_ok=True)
-
     debate_count = 0
 
-    for file in os.listdir(filepath):  # for every file in the given folder
-        if file[-14:] == ' - Results.csv':  # if results file
-            # get file's filepath
-            filepath = Path(folder + file)
-            filepath.parent.mkdir(parents=True, exist_ok=True)
-            df = pd.read_csv(filepath)  # convert file to dataframe
-            debate_count += len(df['Rankings'])  # add number of rooms to debate_count
+    for results_df in results_dfs:  # for every file in the given folder
+        debate_count += len(results_df['Rankings'])  # add number of rooms to debate_count
     return debate_count
 
 
+def get_results_dfs(folder: str) -> List[pd.DataFrame]:
+    results_dfs = []
+
+    for file in os.listdir(folder):  # iterate through all the scraped data
+        if file[-14:] == ' - Results.csv':  # if results file
+            # find the file's filepath and get the dataframe
+            filepath = Path(folder + file)
+            results_df = pd.read_csv(filepath)
+            results_dfs.append(results_df)
+
+    return results_dfs
+
+
+def get_finals_rankings_dfs(results_dfs: List[pd.DataFrame]) -> List[pd.DataFrame]:
+    outrounds_results_dfs = []
+    for results_df in results_dfs:
+        outrounds = ['Final' in round_name or 'pen' in round_name
+                     for round_name in results_df['Round Name']]
+        outrounds_results_df = results_df[pd.Series(outrounds)]
+        outrounds_results_dfs.append(outrounds_results_df)
+    return outrounds_results_dfs
+
+
 if __name__ == '__main__':
-    print(count_debates('scraped_data/'))
+    results_dfs = get_results_dfs('scraped_data/')
+    print(team_positions_stats(results_dfs))
+    #
+    # outrounds_results_dfs = get_finals_rankings_dfs(results_dfs)
+    # print(team_positions_stats(outrounds_results_dfs))
+
+    # import scrape_results
+    # from selenium import webdriver
+    # from selenium.webdriver.chrome.service import Service
+    # s = Service('C:\\Users\\Dell\\.wdm\\drivers\\edgedriver\\win64\\98.0.1108.62\\msedgedriver.exe')
+    # driver = webdriver.Edge(service=s)
+    #
+    # dfs = scrape_results.scrape_all_results(
+    #     'https://oxford2021.calicotab.com/oxfordiv2021/motions/statistics/', driver)
+    # for round in round_by_round_stats(dfs):
+    #     print(round)
+    #
+    # driver.close()
